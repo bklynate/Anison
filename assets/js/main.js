7989099cb7ac22c18c1451d61da484c562ddf666
bklynate <nathanielcfa@gmail.com>
2017-02-16 14:41:44 -0500

    [add] reference anime object created

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,15 @@
-var Anime = require('anime-scraper').Anime
+const Anime = require('anime-scraper').Anime;
+const fs = require('fs');
 
 // You can also search and then choose manually
 Anime.search('boku no hero academia').then(function (results) {
   // Same as above but uses the second search result rather than the first.
   results[1].toAnime().then(function (anime) {
-    console.log(anime)
-  })
-})
+    // console.log(anime);
+    fs.writeFile('anime.json', anime, function(err) {
+      if(err) {
+        console.log(err);
+      }
+    });
+  });
+});
