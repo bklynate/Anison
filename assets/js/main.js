f6023c2f86d6d120794b8f81c4e377f021b93d0b
bklynate <nathanielcfa@gmail.com>
2017-02-16 14:05:10 -0500

    [edit] download one json object from the api

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,7 @@
 var Anime = require('anime-scraper').Anime
 
-// Searches for anime using a POST request & uses first result
-Anime.fromName('Sword Art Online').then(function (anime) {
-  console.log(anime)
-})
-
 // You can also search and then choose manually
-Anime.search('Sword Art Online').then(function (results) {
+Anime.search('boku no hero academia').then(function (results) {
   // Same as above but uses the second search result rather than the first.
   results[1].toAnime().then(function (anime) {
     console.log(anime)
