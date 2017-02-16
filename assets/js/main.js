1835286f7f5e65aee0d7dc39b4fa00f91ffc6430
bklynate <nathanielcfa@gmail.com>
2017-02-16 14:00:41 -0500

    [working] api call to anime db

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,14 @@
-const Anime = require('anime-scraper').Anime;
+var Anime = require('anime-scraper').Anime
 
-Anime.fromName('Haikyuu!!').then(function (anime) {
-  anime.episodes[0].fetch().then(function (episode) {
-    console.log(episode)
+// Searches for anime using a POST request & uses first result
+Anime.fromName('Sword Art Online').then(function (anime) {
+  console.log(anime)
+})
+
+// You can also search and then choose manually
+Anime.search('Sword Art Online').then(function (results) {
+  // Same as above but uses the second search result rather than the first.
+  results[1].toAnime().then(function (anime) {
+    console.log(anime)
   })
-});
+})
