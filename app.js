eb2ce460233ba4ad60771ccc25e45b02ff7eb9e4
bklynate <nathanielcfa@gmail.com>
2017-02-18 17:29:33 -0500

    [edit] working on application backend for video fetching

--- app.js
+++ app.js
@@ -5,13 +5,12 @@ const path = require('path')
 
 app.use(express.static('public'));
 app.set('views', path.join(__dirname, '/views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
 app.set('port', process.env.PORT || 8080);
 
 
-app.get('/', (request, response) => {
-  response.render('index');
-});
+app.get('/', require('./routes').index);
 
 app.listen(app.get('port'), () => {
   console.log('Alive on port', app.get('port'));
