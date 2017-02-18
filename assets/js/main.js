6f0fe93868477ce8d0d1210afa275c0545c11ae4
bklynate <nathanielcfa@gmail.com>
2017-02-17 20:55:57 -0500

    [edit] working api call to gather anime info

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,1 @@
-const Anime = require('anime-scraper').Anime;
-const fs = require('fs');
 
-// You can also search and then choose manually
-Anime.search('boku no hero academia').then(function (results) {
-  // Same as above but uses the second search result rather than the first.
-  results[1].toAnime().then(function (anime) {
-    // console.log(anime);
-    fs.writeFile('anime.json', anime, function(err) {
-      if(err) {
-        console.log(err);
-      }
-    });
-  });
-});
