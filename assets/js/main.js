4363537dcc7e2d6223633eac719eff10e0071d44
bklynate <nathanielcfa@gmail.com>
2017-02-17 21:27:00 -0500

    [add] simple express app wiring

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,13 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
 
+app.set('port', process.env.PORT || 8080)
+
+app.get('/', (request, response) => {
+  response.send('<h1>Knock Knock Mutha.....</h1>')
+});
+
+app.listen(app.get('port'), () => {
+  console.log('Alive on port', app.get('port'));
+})
