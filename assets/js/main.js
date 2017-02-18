e80278747b373b9fab6f0ff35c9ff3ff98ccb7b9
bklynate <nathanielcfa@gmail.com>
2017-02-17 21:44:49 -0500

    [add] pug template package

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,10 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
-app.set('port', process.env.PORT || 8080)
+app.use(express.static('assets'));
+app.set('view engine', 'pug');
+app.set('port', process.env.PORT || 8080);
+
 
 app.get('/', (request, response) => {
   response.send('<h1>Knock Knock Mutha.....</h1>')
