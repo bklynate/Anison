944eb30908af240399a27c6dedf9e93b09f1b06d
bklynate <nathanielcfa@gmail.com>
2017-02-18 12:59:53 -0500

    [edit] fixed application structure

--- public/js/animeBot.js
+++ public/js/animeBot.js
@@ -1,11 +1,17 @@
-var Anime = require('malapi').Anime;
+var Aniscrape = require('aniscrape'); // Check source on GitHub for more info.
+var animebam = require('aniscrape-animebam');
 
-Anime.fromName('boku no hero academia').then(anime => {
-  console.log(anime.episodesLink);
-
-  let boku = anime
-
-  boku.getEpisodes().then(episodes => {
-    console.log("Episodes:",episodes);
-  });
-});
+var scraper = new Aniscrape();
+scraper.use(animebam)
+.then(function() {
+  scraper.search('Haikyuu', 'animebam').then(function (results) {
+    console.log(results)
+    scraper.fetchSeries(results[0]).then(function(anime) {
+      console.log(anime)
+      scraper.fetchVideo(anime.episodes[1]).then(function(video) {
+        // Video is a list of direct video links and quality labels.
+        console.log(video)
+      })
+    })
+  })
+})
