a0b75982ae13309fc7019caf60f9e7ed295df493
bklynate <nathanielcfa@gmail.com>
2017-02-22 18:33:53 -0500

    [finished] able to capture embedded video link

--- app.js
+++ app.js
@@ -3,14 +3,17 @@ const app = express();
 const bodyParser = require('body-parser');
 const path = require('path')
 
-app.use(express.static('public'));
+// line 7 allows for scripts in the public/js to work.
+app.use('/public', express.static(__dirname + '/public'));
 app.set('views', path.join(__dirname, '/views'));
-app.set('view engine', 'jsx');
-app.engine('jsx', require('express-react-views').createEngine());
+app.set('view engine', 'ejs');
+// app.engine('jsx', require('express-react-views').createEngine());
 app.set('port', process.env.PORT || 8080);
 
 
-app.get('/', require('./routes').index);
+app.get('/', (request, response) => {
+  response.render('index')
+})
 
 app.listen(app.get('port'), () => {
   console.log('Alive on port', app.get('port'));
--- public/js/animeBot.js
+++ public/js/animeBot.js
@@ -1,13 +1,9 @@
 const Aniscrape = require('aniscrape'); // Check source on GitHub for more info.
 const animebam = require('aniscrape-animebam');
 const fs = require('fs');
-const phantomjs = require('phantomjs-prebuilt');
-const webdriverio = require('webdriverio');
-const wdOpts = { desiredCapabilities: { browserName: 'phantomjs' } }
-const Horseman = require('node-horseman');
-const horseman = new Horseman();
+const xray = require('x-ray')()
 
-const scraper = new Aniscrape();
+
 scraper.use(animebam)
 .then(function() {
   scraper.search('boku no hero academia', 'animebam').then(function (results) {
@@ -15,15 +11,10 @@ scraper.use(animebam)
     scraper.fetchSeries(results[0]).then(function(anime) {
       // console.log('ANIME:', anime.episodes[0].url)
       let url = anime.episodes[0].url
-      // console.log(url);
-      horseman
-        .userAgent('Chrome/41.0.2228.0')
-        .open(url)
-        .html()
-        .then(html => {
-          console.log(html);
-        })
-        .close();
+      console.log(url);
+      xray(url, 'iframe.embed-responsive-item@src')(function(error, info) {
+        return console.log(info);
+      })
     })
   })
 })
